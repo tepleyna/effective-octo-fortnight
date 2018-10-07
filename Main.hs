d576e67345b09dbaa49e4f55b7acc98c44e49184
Walt Panfil <panfilwk@rose-hulman.edu>
2018-10-07 18:45:26 -0400

    Added initial definition of state data structure

--- Main.hs
+++ Main.hs
@@ -1,14 +1,51 @@
 module Main where
 import Graphics.Gloss
+import Graphics.Gloss.Data.ViewPort
+import Graphics.Gloss.Interface.Pure.Game
+
+fps :: Int
+fps = 60
+
+type Position = (Float, Float)
+type Behavior = Entity -> Entity
+type Weapon = Position -> Entity
+data GameState = State
+  { player :: Entity
+  , foes :: [Entity]
+  , pews :: [Entity]
+  , paused :: Bool
+  }
+data Entity = Entity
+  { position :: Position
+  , behaviors :: [Behavior]
+  , weapon :: Maybe Weapon}
+
+initialState :: GameState
+initialState = State
+  { player = Entity {position = (0,0), behaviors = [goRight], weapon = Nothing}
+  , foes = []
+  , pews = []
+  , paused = False
+  }
+
+goRight :: Behavior
+goRight e = e { position = (x+5, y) }
+ where (x,y) = position e
 
 window :: Display
-window = InWindow "Nice Window" (200, 200) (10, 10)
+window = InWindow "EffectiveOctoFortnite" (200, 200) (10, 10)
 
 background :: Color
-background = white
+background = light $ light blue
 
-drawing :: Picture
-drawing = circle 80
+drawing :: GameState -> Picture
+drawing state = circle 80
+
+update :: Float -> GameState -> GameState
+update ticks state = state
+
+handler :: Event -> GameState -> GameState
+handler _ state = state
 
 main :: IO ()
-main = display window background drawing
+main = play window background fps initialState drawing handler update
